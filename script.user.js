eba994219447d008f67265299b0a5e73d03bc650
jhalff <j_halff@live.nl>
2026-08-19 15:07:33 +0200

    Update version to 1.0.1

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TORN Dividend Planner
 // @namespace    https://github.com/jhalff/torn-dividend-planner
-// @version      1.0.0
+// @version      1.0.1
 // @description  Build and manage TORN stock dividend combinations
 // @author       Draxeth
 // @match        https://www.torn.com/page.php?sid=stocks
