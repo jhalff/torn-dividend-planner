f72a26573310bf8237fecd055064e599e2e16430
jhalff <j_halff@live.nl>
2026-08-19 15:20:59 +0200

    Add more logging

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TORN Dividend Planner
 // @namespace    https://github.com/jhalff/torn-dividend-planner
-// @version      1.0.3
+// @version      1.0.4
 // @description  Build and manage TORN stock dividend combinations
 // @author       Draxeth
 // @match        https://www.torn.com/page.php?sid=stocks
@@ -13,6 +13,8 @@
 
 (function () {
     'use strict';
+
+    alert('TORN Dividend Planner loaded');
 
     const STOCK_SELECTOR = 'ul.stock___CnywB';
     const CONTAINER_SELECTOR = '.stockMarket___IoTUH';
